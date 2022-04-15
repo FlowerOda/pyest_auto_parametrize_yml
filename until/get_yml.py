16cad34e21ad301b892bec81fac7387a14bf06ec
FlowerOda <42458560+FlowerOda@users.noreply.github.com>
2022-04-15 17:04:38 +0800

    Update get_yml.py

--- until/get_yml.py
+++ until/get_yml.py
@@ -10,7 +10,7 @@ def auto_get_yml_data(package_path,name) :
     :param name: 用例名称
     :return: yml对象
     """
-    filepath=os.path.join(package_path,f"data\{os.path.basename(inspect.stack()[2][1]).split('.')[0]}_data.yml")
+    filepath=os.path.join(package_path,f"data/{os.path.basename(inspect.stack()[2][1]).split('.')[0]}_data.yml")
 
     print('yaml data path automatic get ',filepath)
     with open(filepath, encoding='utf-8') as f:
