386801d12f7c0ca02324ae90a6342bbb658ef600
FlowerOda <42458560+FlowerOda@users.noreply.github.com>
2022-04-15 16:39:11 +0800

    Update test_baidu.py

--- case/test_baidu.py
+++ case/test_baidu.py
@@ -1,8 +1,12 @@
 # —*- coding:utf-8 -*-
 import pytest
+from conftest import auto_parametrize_yml
 
 class TestBaiDu:
     @pytest.auto_parametrize_yml()
     def test_search_success(self,key):
         '''注意对应的 yaml文件 里面的值必须是  search_success，看这个case名称和yml名称你就明白咋回事了，约定大于配置 '''
         print(f'搜索关键字为{key}')
+
+if __name__ == '__main__':
+    pytest.main(['-vs',__file__],plugins=[auto_parametrize_yml])
